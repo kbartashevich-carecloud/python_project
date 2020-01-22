bc79963a47e5a6f7fb14faf92e5fbde438e30131
Kiryl <kbartashevich@carecloud.com>
2020-01-22 11:13:18 -0500

    Python flow control

--- example.py
+++ example.py
@@ -1,1 +1,22 @@
-print("hello")
+import random
+
+number = random.randint(0, 100)
+
+while True:
+    answer = input("Guess number: ")
+    if answer == "" or answer == "exit":
+        print("Close program")
+        break
+    if not answer.isdigit():
+        print("Enter number")
+        continue
+
+    answer = int(answer)
+
+    if answer == number:
+        print("Excatly!")
+        break
+    elif answer < number:
+        print("Incorrect number!")
+    else:
+        print("Lower")
