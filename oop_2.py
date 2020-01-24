c1d75935f091309bb7042d8bcc3523c10126b0f6
Kiryl <kbartashevich@carecloud.com>
2020-01-24 17:16:54 -0500

    Added oop inheritance example_2

--- oop_2.py
+++ oop_2.py
@@ -46,6 +46,47 @@ class Dog(Pet):
     def get_breed(self):
         return self.__breed
 
+class PetExport:
+    def export(self, dog):
+        raise NotImplementedError
 
+
+class ExportXML(PetExport):
+    def export(self, dog):
+        return """<?xml version="1.0" encoding="utf-8"?>
+               <dog>
+              <name>{0}</name>
+              <breed>{1}</breed>
+              </dog>""".format(dog.name, dog.breed)
+
+class ExportJSON(PetExport):
+    def export(self, dog):
+        return json.dumps({
+            "name": dog.name,
+            "breed": dog.breed
+        })
+
+class Pet:
+    def __init__(self, name):
+        self,name = name
+class Dog(Pet):
+    def __init__(self, name, breed=None):
+        super().__init__(name)
+        self.breed = breed
+
+class ExDog(Dog):
+    def __init__(self, name, breed=None, exporter=None):
+        super().__init__(name, breed)
+        self._exporter = exporter or ExportJSON()
+
+        if not isinstance(self._exporter, PetExport):
+            raise ValueError("bad export instance value", exporter)
+
+    def export(self):
+        return self._exporter.export(self)
+
+
+dog = ExDog("Jack", "Street", exporter=ExportXML())
+dog.export()
 
 
