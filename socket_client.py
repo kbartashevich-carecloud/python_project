1432b8cab379e6f1995f1a732fc8d93d2b0fa84a
Kiryl <kbartashevich@carecloud.com>
2020-01-28 14:45:12 -0500

    socket server/client

--- socket_client.py
+++ socket_client.py
@@ -1,6 +1,17 @@
 import socket
+#
+# sock = socket.socket()
+# sock.connect(("127.0.0.1", 10001))
+# sock.sendall("ping".encode("utf8"))
+# sock.close()
+#
 
-sock = socket.socket()
-sock.connect(("127.0.0.1", 10001))
-sock.sendall("ping".encode("utf8"))
-sock.close()
+with socket.create_connection(("127.0.0.1", 10001)) as sock:
+    sock.settimeout(2)
+    try:
+        sock.sendall("ping".encode("utf8"))
+    except socket.timeout:
+        print("send data timeout")
+    except socket.error as ex:
+        print("send data error:", ex)
+    sock.close()
--- sockets_python.py
+++ sockets_python.py
@@ -1,15 +1,21 @@
 import socket
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("127.0.0.1", 10001))
-sock.listen(socket.SOMAXCONN)
+def process_request(conn, addr):
+    print("connected client", addr)
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf8"))
 
-conn, addr = sock.accept()
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    print(data.decode("utf8"))
 
-conn.close()
-sock.close()
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    while True:
+        conn, addr =  sock.accept()
+        th = threading.Thread(target=process_request, args=(conn, addr))
+        th.start
+        
