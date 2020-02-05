c84903a4d9770fbfa377a5683e85a13faf014cf4
Kiryl <kbartashevich@carecloud.com>
2020-02-05 13:48:22 -0500

    permutation algos

--- algos.py
+++ algos.py
@@ -20,3 +20,22 @@ def is_unique_2(string):
 
     return True
 
+def is_permutation(str_a="", str_b=""):
+    len_a, len_b = len(str_a), len(str_b)
+
+    if len_a == 0 or len_b == 0 or len_a != len_b:
+        return False
+
+    if sorted(str_a) == sorted(str_b):
+        return True
+
+    return False
+
+
+def urlify(string=""):
+    if not string:
+        return string
+
+    return string.strip().replace(" ", "%20")
+
+print(urlify("Mr John Smith    "))
